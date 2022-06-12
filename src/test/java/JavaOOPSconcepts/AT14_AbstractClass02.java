1a234363a09b1e0d25fa0da12afe0b35435e7df3
Jagan Etikala <Jagan.Etikala@tabcorp.com.au>
2022-06-13 08:15:53 +1000

    selenium

---
+++ src/test/java/JavaOOPSconcepts/AT14_AbstractClass02.java
@@ -0,0 +1,14 @@
+package JavaOOPSconcepts;
+
+import org.junit.Test;
+
+public class AT14_AbstractClass02 extends AT13_AbstractClass01{
+
+    @Test
+    public void divOfNum(){
+        int x=1000;
+        int y=200;
+        System.out.println("Div of 2 numbers is : "+(x/y));
+    }
+
+}
---
+++ src/test/java/SeleniumLearning/AT01_NavigateTitleAssertScreenshot.java
@@ -0,0 +1,45 @@
+package SeleniumLearning;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class AT01_NavigateTitleAssertScreenshot {
+    @Test
+    public void NavtoWebPage() throws IOException {
+        //setting up chromedriver
+        WebDriverManager.chromedriver().setup();
+        //we create an object of WebDriver
+        WebDriver driver = new ChromeDriver();
+        //At this point we have a empty browser opened
+        //maximize the browser
+        driver.manage().window().maximize();
+        //Navigate to webpage
+        driver.get("https://www.harveynorman.com.au/");
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //get title of webpage
+        String ActualTitleName=driver.getTitle();
+        System.out.println("ActualTitleName : "+ActualTitleName);
+        String ExpectedTitle="Harvey Norman | Shop Online for Computers, Electrical, Furniture, Bedding, Bathrooms & Flooring | Harvey Norman Australia";
+//Assertion Version-1
+        Assert.assertEquals(ExpectedTitle,ActualTitleName);
+//Assert version-2
+        Assert.assertTrue(ActualTitleName.contains("Computers"));
+
+        //Screenshot
+        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotFile, new File("C:\\Users\\etikalaj\\screenshot.png"));
+
+    }
+}
